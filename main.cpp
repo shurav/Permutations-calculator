850132772b179945d4dd3f29aa18fa60871948c4
Shurav Nandy <nandyshurav@gmail.com>
2021-08-21 17:38:39 -0400

    c++ file

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int factorialFinder(int x)
+{
+    if (x==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return x*factorialFinder(x-1);
+    }
+}
+
+int permutationsCalculator(int x, int y)
+{
+    int n = factorialFinder(x);
+    int z = factorialFinder(x-y);
+    return (n/z);
+}
+
+int main()
+{
+    int n, k;
+    cout << "This is a permutations calculator" << endl;
+    cout << "Give the value for n" << endl;
+    cin >> n;
+    cout << "Give the value for k" << endl;
+    cin >> k;
+    cout << "The value of nPk is " << permutationsCalculator(n, k) << endl;
+    return 0;
+}
